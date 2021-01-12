9213b7db13b2e3b2e1cd6d94f9ee6d7f01687a68
David mochoge <47295684+kokodavid@users.noreply.github.com>
2021-01-12 11:32:32 +0300

    Ft search (#3)
    * Search Functionality
    
    * Chatroom creation Functionality
    
    * search functionality complete

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -28,5 +28,30 @@ class DatabaseMethods{
           print(e.toString());
     });
     }
+    
+    addConversationMessages(String chatRoomId, messageMap){
+       FirebaseFirestore.instance.collection("ChatRoom")
+          .doc(chatRoomId)
+          .collection("chats")
+          .add(messageMap).catchError((e){
+            print(e.toString());
+      });
+    }
+
+    getConversationMessages(String chatRoomId)async{
+      return await FirebaseFirestore.instance.collection("ChatRoom")
+          .doc(chatRoomId)
+          .collection("chats")
+          .orderBy("time",descending: false)
+          .snapshots();
+    }
+
+    getChatRooms(String userName)async{
+    return await FirebaseFirestore.instance.collection("ChatRoom")
+    .where("users", arrayContains: userName)
+        .snapshots();
+
+    }
+    
 
 }
